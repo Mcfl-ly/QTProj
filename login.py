5f9336fa4fda54d2d1c5484b38aa44c4aae079de
Gabriel <gab.lyma7@gmail.com>
2025-12-16 19:22:01 -0300

    otimização da tela de login

--- login.py
+++ login.py
@@ -9,22 +9,47 @@ class LoginScreen(QWidget):
     def __init__(self, stacked_widget):
         super().__init__()
         self.stacked_widget = stacked_widget
-        self.setFixedSize(540,360)
-        self.setStyleSheet("background-color: transparent;")
 
-        layout = QVBoxLayout()
+        #cria o layout geral do widget de login
+        tela_layout = QGridLayout(self)
+        tela_layout.setContentsMargins(0, 0, 0, 0)
+        tela_layout.setSpacing(0)
 
+        #aqui cria a telinha de login, a parte importante
+        self.screen = QWidget()
+        self.screen.setObjectName("screen")
+        self.screen.setFixedSize(540,360)
 
+        #cria o layout da telinha de login, colocando ele na vertical
+        screen_layout = QVBoxLayout(self.screen)
+        screen_layout.setContentsMargins(24,24,24,24)
+        screen_layout.setSpacing(16)
+
+        #textos das caixas
         self.login_textbox = TextBox()
         self.login_textbox.setPlaceholderText("Login")
-
         self.password_textbox = TextBox()
         self.password_textbox.setPlaceholderText("Password")
         self.password_textbox.setEchoMode(TextBox.EchoMode.Password)
 
+        #adicionando as caixas de texto a tela de login
+        screen_layout.addWidget(self.login_textbox)
+        screen_layout.addWidget(self.password_textbox)
 
+        #adicionando a telinha de login ao widget inteiro e alinhando ao centro
+        tela_layout.addWidget(self.screen,1,1,alignment=Qt.AlignmentFlag.AlignCenter)
+        tela_layout.setRowStretch(0, 1)
+        tela_layout.setRowStretch(2, 1)
+        tela_layout.setColumnStretch(0, 1)
+        tela_layout.setColumnStretch(2, 1)
 
-        layout.addWidget(self.login_textbox, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(self.password_textbox, alignment=Qt.AlignmentFlag.AlignCenter)
-        self.setLayout(layout)
+        self.setStyleSheet("background-color: yellow;")
 
+        self.setStyleSheet("""
+                   QWidget { background-color: #1e1e1e; }
+                   #screen {
+                       background-color: transparent;
+                       border-radius: 12px;
+                       border: 2px solid #ff1d8d;
+                   }
+               """)
